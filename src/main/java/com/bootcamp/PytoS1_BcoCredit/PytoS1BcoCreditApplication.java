23efac370fec6936511b54651988d7faab888d09
Burgoos <alexanderburgossaba@gmail.com>
2022-03-13 08:46:59 -0500

    First commit

---
+++ src/main/java/com/bootcamp/PytoS1_BcoCredit/PytoS1BcoCreditApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.PytoS1_BcoCredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PytoS1BcoCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PytoS1BcoCreditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/PytoS1_BcoCredit/PytoS1BcoCreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.PytoS1_BcoCredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PytoS1BcoCreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
